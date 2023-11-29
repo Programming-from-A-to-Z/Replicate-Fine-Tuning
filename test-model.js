3de3f69c24277b4319fe979a2105106a82b7cd4e
Daniel Shiffman <daniel.shiffman@gmail.com>
2023-11-28 22:01:34 -0500

    testing the model and env sample

--- test-model.js
+++ test-model.js
@@ -6,29 +6,15 @@ const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
 });
 
+const version = 'c36e2630dd1471609acd290f9cfeb92a47f2419f6695481b07154408ab87d3b7';
+const model = 'itpnyu/sample-finetune-processing-course';
+
 go();
 
 async function go() {
-  const output = await replicate.run(
-    'shiffman/raanapbot:d0a4ffcc79af3f098340574b63b371a2c992f5931f62438e2e7ab1d780b8178c',
-    {
-      input: {
-        prompt: 'mailbag ->',
-      },
-    }
-  );
-  console.log(output);
+  const input = {
+    prompt: 'Drawing a circle in Processing is as easy as...',
+  };
+  const output = await replicate.run(`${model}:${version}`, { input });
+  console.log(output.join('').trim());
 }
-
-// async function go() {
-//   const output = await replicate.run(
-//     'zeke/nyu-llama-2-7b-chat-training-test:aae0f2ef9dd402d20aba3e83adcbb7ab8fb55fdc3081d14abb6aa082b181c981',
-//     {
-//       input: {
-//         prompt: 'mailbag ->',
-//         system_prompt: '',
-//       },
-//     }
-//   );
-//   console.log(output.join(''));
-// }
